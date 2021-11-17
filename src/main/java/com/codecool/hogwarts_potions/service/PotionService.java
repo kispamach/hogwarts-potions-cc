69aad541f7c0f204f21ccd8a928470fc5d0d5320
kispamach <fazekasdav@gmail.com>
2021-11-17 19:16:11 +0100

    Implemented Potion brewing status

--- src/main/java/com/codecool/hogwarts_potions/service/PotionService.java
+++ src/main/java/com/codecool/hogwarts_potions/service/PotionService.java
@@ -1,7 +1,11 @@
 package com.codecool.hogwarts_potions.service;
 
+import com.codecool.hogwarts_potions.model.BrewingStatus;
+import com.codecool.hogwarts_potions.model.Ingredient;
 import com.codecool.hogwarts_potions.model.Potion;
+import com.codecool.hogwarts_potions.model.Recipe;
 import com.codecool.hogwarts_potions.repository.PotionRepository;
+import com.codecool.hogwarts_potions.repository.RecipeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +14,47 @@ import java.util.List;
 public class PotionService {
 
     private PotionRepository potionRepository;
+    private RecipeRepository recipeRepository;
 
     public PotionService(PotionRepository potionRepository) {
         this.potionRepository = potionRepository;
     }
 
+    private void checkBrewingStatus(Potion potion) {
+
+        if (potion.getIngredients().size() < 5) {
+            potion.setBrewingStatus(BrewingStatus.BREW);
+        }
+        else if (checkRecipeIngredients(potion.getIngredients()) != null) {
+            potion.setBrewingStatus(BrewingStatus.REPLICA);
+        }
+        else {
+            potion.setBrewingStatus(BrewingStatus.DISCOVERY);
+        }
+    }
+
+    private Recipe checkRecipeIngredients(List<Ingredient> ingredients) {
+        List<Recipe> recipes = recipeRepository.findAll();
+        return recipes
+                .stream()
+                .filter(recipe -> recipe.getIngredients().equals(ingredients))
+                .findFirst()
+                .orElse(null);
+
+//        return recipes
+//                .stream()
+//                .filter(recipe -> ingredients
+//                        .stream()
+//                        .allMatch(ingredient -> ingredient.getId().equals(recipe.getIngredients()
+//                                .stream()
+//                                .map(Ingredient::getId))))
+//                .findFirst()
+//                .orElse(null);
+
+    }
+
     public List<Potion> getAllPotions() {
         return potionRepository.findAll();
     }
+
 }
