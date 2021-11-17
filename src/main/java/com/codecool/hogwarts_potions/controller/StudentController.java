38a88f6dd5eadca97778bbb280bcf3d039c33ab4
kispamach <fazekasdav@gmail.com>
2021-11-17 17:08:33 +0100

    Implemented student service

---
+++ src/main/java/com/codecool/hogwarts_potions/controller/StudentController.java
@@ -0,0 +1,47 @@
+package com.codecool.hogwarts_potions.controller;
+
+import com.codecool.hogwarts_potions.model.Room;
+import com.codecool.hogwarts_potions.model.Student;
+import com.codecool.hogwarts_potions.service.RoomService;
+import com.codecool.hogwarts_potions.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student){
+        studentService.addStudent(student);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable("id") Long id){
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateStudentById(@PathVariable("id") Long id, @RequestBody Student updatedStudent){
+        studentService.updateStudentById(id, updatedStudent);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudentById(@PathVariable("id") Long id){
+        studentService.deleteStudentById(id);
+    }
+}
---
+++ src/main/java/com/codecool/hogwarts_potions/service/StudentService.java
@@ -0,0 +1,44 @@
+package com.codecool.hogwarts_potions.service;
+
+import com.codecool.hogwarts_potions.model.Student;
+import com.codecool.hogwarts_potions.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public void setStudentRepository(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student) {
+        if(student != null) studentRepository.save(student);
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public void updateStudentById(Long id, Student updatedStudent) {
+        Student student = getStudentById(id);
+        if (student != null && updatedStudent != null) {
+            student.setName(updatedStudent.getName());
+            student.setHouseType(updatedStudent.getHouseType());
+            student.setPetType(updatedStudent.getPetType());
+        }
+    }
+
+    public void deleteStudentById(Long id) {
+        studentRepository.findById(id).ifPresent(student -> studentRepository.delete(student));
+    }
+}
